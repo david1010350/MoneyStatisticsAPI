e88e8b3f75d562eb208c583042aa3eb56889cea6
david1010350 <cmt1470@gmail.com>
2026-09-13 22:21:37 +0800

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,12 @@
+// ========================================
+// Supabase 設定
+// ========================================
+
+const SUPABASE_URL = "https://hztxqrfuqgdktxaushfo.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_GT64xGBTDavCDwxEolZrlA_63WIx7_W";
+
+// 建立 Supabase Client
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+);
